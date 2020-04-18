a43a3c407412a7309aa6ed80e4bc53dc0dc5310d
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-04-17 22:40:34 -0400

    Allow updating only U or V from sp_nnmf

--- nnmf_k_ver.m
+++ nnmf_k_ver.m
@@ -1,4 +1,4 @@
-function  BCV = nnmf_k_ver( A, num, sets, kmin, kmax)
+function  BCV = nnmf_k_ver( A, num, sets, kmin, kmax, conv, maxiter)
 %Bi-cross validation of rank of NNMF according to the Algorithm 3 of http://arxiv.org/pdf/0908.2062v1.pdf
 %Inputs: A is the original matrix to be factorized, num is a 2 element
 %vector that contains the number of rows and columns to hold out of the
@@ -7,6 +7,14 @@ function  BCV = nnmf_k_ver( A, num, sets, kmin, kmax)
 %maximum rank. 
 %Outputs: BCV is a matrix of dimensions setsXlength(kmin:kmax) where each
 %column contains the reconstruction error for a given value of k and each row is each individual set.
+
+if nargin < 6 || isempty(conv)
+    conv = 1e-5;
+end
+
+if nargin < 7 || isempty(maxiter)
+    maxiter = 5000;
+end
 
 [n,m]=size(A);
 rowHold=zeros(num(1), sets);
@@ -38,100 +46,14 @@ for j=1:sets
 % parallel loop for all values of K
     parfor i=1:length(k)
         % algo 3 in http://arxiv.org/pdf/0908.2062v1.pdf
-        [U_tilde, V_tilde, ~]=sp_nnmf(A_rc,k(i), 0);            % regular NNMF. sparsity constraint removed
-        U_hat=U_onlyNMF(A_c, V_tilde, k(i));                    % NNMF holding V constant
-        V_hat=V_only_NMF(A_r, U_tilde, k(i));                   % NNMF holding U constant
+        [U_tilde, V_tilde, ~]=sp_nnmf(A_rc,k(i), 0, conv, maxiter); % regular NNMF. sparsity constraint removed
+        U_hat=sp_nnmf(A_c, k(i), 0, conv, maxiter/10, [], V_tilde); % NNMF holding V constant
+        [~, V_hat]=sp_nnmf(A_r, k(i), 0, conv, maxiter/10, U_tilde);% NNMF holding U constant
         BCV(j, i)=pen(A, U_hat, V_hat);                         % Frobenius norm of the difference between full and K-ranked reconstruction
     end
 
 end
 
-
-end
-
-function U=U_onlyNMF(A,V, k)
-maxiter=500;
-conv=0.00001; 
-p=zeros(maxiter,1);
-
-[n, m]=size(A);
-
-% initialize U
-U=rand(n,k);
-improv=10;
-% dummie value to enter the loop
-improv=10;
-%initialize count of iterations
-counter=0;
-
-while abs(improv)>conv || improv<0 
-    U=uUpdate(U,V,A);
-    p(counter+1)=pen(A, U, V); 
-    if counter>=1
-        improv=(p(counter)-p(counter+1))./p(counter);
-    end
-    counter=counter+1;
-    if counter>=maxiter
-     %   disp('Maximum number of iterations reached without convergence');
-        improv=0;   % kill the loop
-    end
-
-end
-% truncate to the actual number of iterations
-p=p(1:counter);
-end
-
-
-function V=V_only_NMF(A, U, k)
-maxiter=500;
-conv=0.00001; 
-p=zeros(maxiter,1);
-
-[~, m]=size(A);
-
-% initialize U
-V=rand(m,k);
-improv=10;
-% dummie value to enter the loop
-improv=10;
-%initialize count of iterations
-counter=0;
-while abs(improv)>conv || improv<0 
-    V=vUpdate(U,V,A);
-    p(counter+1)=pen(A, U, V); 
-    if counter>=1
-        improv=(p(counter)-p(counter+1))./p(counter);
-    end
-    counter=counter+1;
-    if counter>=maxiter
-       % disp('Maximum number of iterations reached without convergence');
-        improv=0;   % kill the loop
-    end
-
-end
-% truncate to the actual number of iterations
-p=p(1:counter);
-end
-
-
-
-
-
-
-function Un=uUpdate(U,V, A)
-% as in Algo 1 of the paper
-num=A*V;
-den=U*(V'*V);
-Un=U.*(num./den);
-Un(isnan(Un))=0;
-end
-
-function Vn=vUpdate(U,V,A)
-% as in Algo 1 of the paper
-num=A'*U;
-den=V*(U'*U);
-Vn=V.*(num./den);
-Vn(isnan(Vn))=0;
 
 end
 
